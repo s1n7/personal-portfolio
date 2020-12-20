ad602acd11ba085cdfd59f6959a86b86cda08702
Luis Brümmer <luisbruemmer@gmail.com>
2020-12-20 18:58:25 +0100

    Database setup

---
+++ portfolio/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Project(models.Model):    #inherit from model class
+    title = models.CharField(max_length=100)        #information in documentation
+    description = models.CharField(max_length=250)
+    image = models.ImageField(upload_to='portfolio/images/')    #all images uploaded to this folder
+    url = models.URLField(blank=True)   #blank by default False
