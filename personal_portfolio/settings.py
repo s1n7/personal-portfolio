c09acde977e03e8f9b6e03474276fcc1502f06a0
Luis Brümmer <luisbruemmer@gmail.com>
2020-12-31 14:08:36 +0100

    Local settings

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -124,3 +124,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'       #url for media
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')       #where to save the media
+
+try:
+    from .local_settings import *
+except ImportError:
+    print("Looks like no local file. You must be on production")
