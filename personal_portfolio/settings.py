c9a0000d4f8f145878a33ac58f3a0c5c0cc2370b
Luis <luisbruemmer@8e86bfb0e784>
2020-12-31 12:24:59 +0000

    Remove old stuff

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*yawcecuga2#tkv8p$e#3_w!9f+8taly2susg+6&ar85&@vc*c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['luisbruemmer.pythonanywhere.com']
 
 
 # Application definition
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'       #url for media
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')       #where to save the media
